dc70cbcc44b6c356d1a5aa99b15bc94b3dd2a1fd
Karen Soler <kjsoler19@misena.edu.co>
2023-11-30 13:56:51 -0500

    FONTS

---
+++ app/src/main/java/com/example/storymaker/NewScene.java
@@ -0,0 +1,13 @@
+package com.example.storymaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewScene extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_scene);
+    }
+}
