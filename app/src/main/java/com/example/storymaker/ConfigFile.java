185096ce202588afea06720281665c57f34e0644
Karen Soler <kjsoler19@misena.edu.co>
2023-12-01 08:17:50 -0500

    CREATED ACTIVITY_TEAM_FILE

---
+++ app/src/main/java/com/example/storymaker/ConfigFile.java
@@ -0,0 +1,14 @@
+package com.example.storymaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigFile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_file);
+    }
+}
